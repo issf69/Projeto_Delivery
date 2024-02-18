3b06ce99c296c2d94db6d15c45dcf4c82c4f87e6
Irene <irenesilvafranca2016@gmail.com>
2024-02-18 07:50:39 -0300

    initial commit

---
+++ pedido/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from .views import efetuar_pedido
+
+urlpatterns = [
+    path("finalizar_pedido/", views.finalizar_pedido, name='finalizar_pedido'),
+    path("validaCupom/", views.validaCupom, name='validaCupom'),
+     path('efetuar_pedido/', efetuar_pedido, name='efetuar_pedido'),
+
+]
