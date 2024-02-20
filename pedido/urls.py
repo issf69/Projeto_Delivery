76b959544d7bb62c32cd064f35e69c03c034b11d
Irene Silva França <105497075+issf69@users.noreply.github.com>
2024-02-20 14:48:58 -0300

    Update urls.py

--- pedido/urls.py
+++ pedido/urls.py
@@ -5,6 +5,6 @@ from .views import efetuar_pedido
 urlpatterns = [
     path("finalizar_pedido/", views.finalizar_pedido, name='finalizar_pedido'),
     path("validaCupom/", views.validaCupom, name='validaCupom'),
-    path('efetuar_pedido/', efetuar_pedido, name='efetuar_pedido'),
+     path('efetuar_pedido/', efetuar_pedido, name='efetuar_pedido'),
 
 ]
