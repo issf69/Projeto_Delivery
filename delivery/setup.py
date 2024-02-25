4660eed56f58d6ee8158c5d22a8d5eded204fd6f
Irene <irenesilvafranca2016@gmail.com>
2024-02-25 07:03:29 -0300

    Adicionando arquivo requirements.txt

---
+++ delivery/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='meu_pacote',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pacote1',
+        'pacote2',
+    ],
+    author='irene',
+    author_email='irenesilvafranca2016@gmail.com',
+    description='Uma breve descrição do seu pacote',
+    url='https://github.com/issf69/Projeto_Delivery.git',
+)
